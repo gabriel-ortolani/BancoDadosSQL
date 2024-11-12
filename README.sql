b1d9421cded274d9afa2f7cf6c87ef01d60f11dd
gabriel-ortolani <gabrielortolanisenai@gmail.com>
2024-11-12 13:29:07 -0300

    arquivo sql

---
+++ README.sql
@@ -0,0 +1,174 @@
+# Passo a passo: Criação de um banco de dados
+Tutorial de como criar um banco de dados SQL que organiza as informações de 'livros', 'ditora', 'autora' e 'assuntos'.
+Esse guia sera dividido em etapas para demonstrar desde a crição de tabelas, chaves e até manipulação/consulta de dados.
+
+## Resumo de uma estrutura SQL
+* __CREATE__ para criar 'Banco de dados' ou 'tabelas'
+* __ALTER__ para adicionar ou modificar colunas
+* __DROP__ para remover 'Banco de dados' ou 'Tabelas'
+* __INSERT__ para adicionar registros na tabela
+* __UPDATE__ para atualizar os registros 
+* __DELETE__ para remover os registros
+* __SELECT__ para consultar e visualizar dados
+
+## Passo 1: criação do Banco de Dados e das Tabelas
+#### 1.1 Criando o DB
+
+```SQL
+CREATE DATABASE biblioteca;
+USE biblioteca;
+```
+
+#### 1.2 Criando a tabela 'editora'
+```SQL
+CREATE TABLE editora (
+    id_editora INT AUTO_INCREMENT PRIMARY KEY,
+    nome_editora VARCHAR(100) NOT NULL,
+    pais VARCHAR(50)
+);
+```
+
+#### 1.3 Criando a tabela 'autor'
+```SQL
+CREATE TABLE autor (
+    id_autor INT AUTO_INCREMENT PRIMARY KEY,
+    nome_autor VARCHAR(100) NOT NULL,
+    data_nascimento DATE
+);
+```
+
+#### 1.4 Criando a tabela 'assunto'
+```SQL
+CREATE TABLE assunto (
+    id_assunto INT AUTO_INCREMENT PRIMARY KEY,
+    descricao_assunto VARCHAR(300) NOT NULL
+);
+```
+
+#### 1.5 Criando a tabela 'livro'
+```SQL
+CREATE TABLE livro (
+    id_livro INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(150) NOT NULL,
+    ano_publicacao INT,
+    editora INT,
+    autor INT,
+    assunto INT,
+    FOREIGN KEY(editora) REFERENCES editora(id_editora),
+    FOREIGN KEY(autor) REFERENCES autor(id_autor),
+    FOREIGN KEY(assunto) REFERENCES assunto(id_assunto)
+);
+```
+
+#### 1.6 Criando uma tabela EXTRA
+A tabela EXTRA vai servir para exemplificar a exclusão
+
+```SQL
+CREATE TABLE extra(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    produtos VARCHAR(50),
+    quantidade INT(20),
+    preco DOUBLE NOT NULL
+);
+```
+
+## Passo 2: editar tabelas usando 'ALTER'
+Após a criação da tabela, podemos adicionar novos campos. Vamos adicionar uma coluna 'email' na tabela 'autor'
+
+```SQL
+ALTER TABLE autor
+ADD COLUMN email VARCHAR(100);
+```
+
+## Passo 3: Remover tabela usando 'DROP'
+Se precisar remover uma tabela usamos o comando 'DROP.
+Neste exemplo vamos remover a tabela 'extra'
+
+```SQL
+DROP TABLE extra;
+```
+
+## Passo 4: Inserindo dados usando 'INSERT'
+Agora que as tabelas já estão prontas, vamos inserir dados nelas.
+
+### Passo 4.1 Inserindo dados na tabela 'editora'
+```SQL
+INSERT INTO editora(nome_editora, pais)
+VALUES
+('Editora Alfa', 'Brasil'),
+('Editora Beta', 'Portugal'),
+('Editora Bertrand Brasil', 'Brasil');
+```
+
+#### 4.2 Inserindo dados na tabela 'autor'
+```SQL
+INSERT INTO autor(nome_autor, data_nascimento, email)
+VALUES
+('Jorge Amado', '1912-08-10', 'jorjinho@email.com'),
+('Machado de Assis', '1839-06-21', 'machadinho@email.com'),
+('Matt Haig', '1975-06-03', 'matt@email.com');
+```
+
+#### 4.3 Inserindo dados na tabela 'assunto'
+```SQL
+INSERT INTO assunto(descricao_assunto)
+VALUES
+('Ficção'),
+('Mistério'),
+('Terror'),
+('Ação');
+```
+
+#### 4.4 Inserindo dados na tabela 'livro'
+```SQL
+INSERT INTO livro(titulo, ano_publicacao, editora, autor, assunto)
+VALUES
+('Capitães da Areia', '1937', 1, 1, 5),
+('Dom Casmurro', '1899', 2, 2, 5),
+('A Biblioteca da Meia-Noite', '2020', 3, 3, 2),
+('Memórias Póstumas de Brás Cubas', '1881', 1, 2, 5);
+```
+
+## Passo 5: Atualizando os dados usando 'UPDATE'
+Podemos atualizar os dados com o comando UPDATE.
+Vamos corrigir a data de publicação do livro 'Capitães da Areia'
+```SQL
+UPDATE livro
+SET ano_publicacao = 1938
+WHERE titulo = 'Capitães da Areia';
+```
+
+## Passo 6: Excluindo os dados usando 'DELETE'
+Para remover os registros de uma tabela usamos o comando 'DELETE'.
+vamos excluir o livro 'Memórias póstumas de Brás Cubas'.
+```SQL
+DELETE FROM livro
+WHERE id_livro = 16;
+```
+
+## Passo 7: Consultando os dados usando 'SELECT'
+É possivel selecionar os dados para visualizar da forma como quiser.
+para isso usamos o comando 'SELECT'
+#### 7.1: Selecionar todos os livros com suas editoras e autores
+Vamos usar os dados das tabelas 'livro', 'editora', 'autor' e 'assunto' usando o comando 'JOIN'
+```SQL
+SELECT  livro.titulo AS titulo,
+        editora.nome_editora AS editora,
+        autor.nome_autor AS autor,
+        assunto.descricao_assunto AS assunto,
+        livro.ano_publicacao AS ano
+FROM livro 
+JOIN editora ON livro.editora = editora.id_editora
+JOIN autor ON livro.autor = autor.id_autor
+JOIN assunto ON livro.assunto = assunto.id_assunto
+```
+
+#### 7.2: Exibir apelas livros com o mesmo tema
+Para selecionar todos os livros que pentencem ao mesmo assunto, podemos fazer uma consulta utilizando o comando 'SELECT' com uma condição 'WHERE' especificando o que deseja visualizar.
+```SQL
+SELECT  livro.titulo AS titulo,
+        assunto.descricao_assunto AS assunto
+FROM livro 
+JOIN assunto ON livro.assunto = assunto.id_assunto
+WHERE assunto.id_assunto = 'Romance';
+```
